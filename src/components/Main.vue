5a3b8a68896f6807281aaa7162798adb51777395
Auralcat <ashleyzt@hi2.in>
2018-07-31 12:27:57 -0300

    Create calculator buttons

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="results">
     <input class="input" name="" type="text" value="" v-model="current"/>
+    <div class="buttons">
+      <button class="button" v-on:click="press">7</button>
+      <button class="button" v-on:click="press">8</button>
+      <button class="button" v-on:click="press">9</button>
+      <button class="button" v-on:click="press">4</button>
+      <button class="button" v-on:click="press">5</button>
+      <button class="button" v-on:click="press">6</button>
+      <button class="button" v-on:click="press">1</button>
+      <button class="button" v-on:click="press">2</button>
+      <button class="button" v-on:click="press">3</button>
+      <button class="button equal-sign" v-on:click="press">=</button>
+      <button class="button" v-on:click="press">C</button>
+      <button class="button" v-on:click="press">*</button>
+      <button class="button" v-on:click="press">/</button>
+      <button class="button" v-on:click="press">√</button>
+      <button class="button" v-on:click="press">x²</button>
+    </div>
   </div>
 </template>
 
