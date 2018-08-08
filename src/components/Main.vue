1bcd61ec10a7781584b8c5883610f98f4858d6ef
Auralcat <ashleyzt@hi2.in>
2018-08-08 17:47:11 -0300

    Fix indentation

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -15,47 +15,47 @@
 </template>
 
 <script>
-  import Visor from "@/components/Visor"
-  import CalcButton from "@/components/Button"
-  import ButtonPanel from "@/components/ButtonPanel"
-  import Switches from 'vue-switches'
+import Visor from "@/components/Visor"
+import CalcButton from "@/components/Button"
+import ButtonPanel from "@/components/ButtonPanel"
+import Switches from 'vue-switches'
 
-  export default {
-    name: 'Main',
-    data () {
-      return {
-        changeMode: true,
-        numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '.'],
-        basicOperations: ["+", "-", "*", "/", "C" , "←", "="],
-        advancedOperations: ["ln", "x²", "^", "ϖ", "√", "log"]
-      }
-    },
-    methods: {
-    },
-    components: {
-      Visor,
-      CalcButton,
-      ButtonPanel,
-      Switches
+export default {
+  name: 'Main',
+  data () {
+    return {
+      changeMode: true,
+      numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '.'],
+      basicOperations: ["+", "-", "*", "/", "C" , "←", "="],
+      advancedOperations: ["ln", "x²", "^", "ϖ", "√", "log"]
     }
+  },
+  methods: {
+  },
+  components: {
+    Visor,
+    CalcButton,
+    ButtonPanel,
+    Switches
   }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-  .buttons {
-    display: flex;
-    justify-content: center;
-  }
+.buttons {
+  display: flex;
+  justify-content: center;
+}
 
-  .calculator-shell {
-    display: inline-table;
-    text-align: center;
-    background-color: #ffc1e7;
-    width: 25vw;
-    margin: auto;
-    padding: 12px;
-    box-shadow: 0 4px 2px darken(#ffc1e7, 5%);
-  }
+.calculator-shell {
+  display: inline-table;
+  text-align: center;
+  background-color: #ffc1e7;
+  width: 25vw;
+  margin: auto;
+  padding: 12px;
+  box-shadow: 0 4px 2px darken(#ffc1e7, 5%);
+}
 
 </style>
--- src/components/Visor.vue
+++ src/components/Visor.vue
@@ -3,27 +3,27 @@
 </template>
 
 <script>
-  import store from "@/store"
+import store from "@/store"
 
-  export default {
-    computed: {
-      currentDisplay () {
-        return store.state.current
-      }
+export default {
+  computed: {
+    currentDisplay () {
+      return store.state.current
     }
   }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
-  input {
-    border: none;
-    border-radius: 4px;
-    color: #372382;
-    padding: 12px;
-    font-family: Orbitron;
-    font-size: 16px;
-    text-align: right;
-    background-color: lighten(#Afeeee, 13%);
-  }
+input {
+  border: none;
+  border-radius: 4px;
+  color: #372382;
+  padding: 12px;
+  font-family: Orbitron;
+  font-size: 16px;
+  text-align: right;
+  background-color: lighten(#Afeeee, 13%);
+}
 </style>
