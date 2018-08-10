de88a3b1f720d70df3d9d0f14a45ce95ad357b4a
Auralcat <ashleyzt@hi2.in>
2018-08-10 14:51:53 -0300

    Refactor tests in Button.spec.js

--- tests/unit/Button.spec.js
+++ tests/unit/Button.spec.js
@@ -3,13 +3,15 @@ import { shallowMount } from '@vue/test-utils'
 import Button from '@/components/Button.vue'
 import store from '@/store'
 
+const factory = (values = {}) => {
+  return shallowMount(Button, {
+    propsData: { ...values }
+  })
+}
+
 describe('Button.vue', () => {
   it('adds a number to the visor when clicked and if it has a number as label', () => {
-    const wrapper = shallowMount(Button, {
-      propsData: {
-        label: '7'
-      }
-    })
+    const wrapper = factory({ label: '7'})
     const button = wrapper.find('button')
     button.trigger('click')
     expect(store.state.current).to.equal('7')
